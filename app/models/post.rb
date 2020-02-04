b5f4a9b65e1478dd00561dbe71c2a89e9900c31e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-04 00:57:57 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
-  validates :content, length: { mininum: 250 }
+  validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ("Fiction" || "Non-Fiction")}
 end
