a72741a1bdcaa940df1b9a9777f50247458e873f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-04 01:04:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  
+  def clickbait
 end
